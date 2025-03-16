3193cdae2389bbbe0d9c0dfa26b4eaa305b44ca9
Tobias <ranft_tobias@web.de>
2025-03-16 17:25:41 +0100

    fix

--- code/sonarMapperMain.py
+++ code/sonarMapperMain.py
@@ -20,8 +20,7 @@ pathToR='"C:/Program Files/R/R-4.2.1/bin/x64/Rscript.exe"'
 # get path from command line argument
 pathToConfig=sys.argv[1]
 # set path in source code
-#pathToConfig="PATH/config.ini"  #if you want to run it from the editor you can set the path to the config here
-#pathToConfig="C:/Users/EddiH/Downloads/SonarData/git/SonarMapper/example/config.ini"
+pathToConfig="PATH/config.ini"  #if you want to run it from the editor you can set the path to the config here
 
 selection,coord= dt.readConfig(pathToConfig)
 
